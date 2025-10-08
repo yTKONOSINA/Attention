f2e0f9d3273a999ea215bbe1e74d33bad530ac23
yTKONOSINA <tsutskov6@gmail.com>
2025-10-08 10:13:35 +0300

    rewritten the linear class with tensors

---
+++ linear.py
@@ -0,0 +1,31 @@
+from tensor import Tensor
+
+class Linear:
+    # modify it to include batches
+    def __init__(self, m : int, n : int, bias : bool = True, 
+                 data : float = 0):
+        self.m = m
+        self.n = n
+        self.w = Tensor([[data for i in range(n)] for j in range(m)])
+        self.bias = bias
+        if bias:
+            self.b = Tensor([data for i in range(n)])
+        else:
+            self.b = None
+    
+    def forward(self, x : list) -> list:
+        y = x @ self.w
+        
+        # Broadcast the bias to every batch
+        if self.bias:
+            y.tensor = [
+                [y_row[i] + self.b.tensor[i] for i in range(self.n)]
+                for y_row in y.tensor
+            ]
+        return y
+
+if __name__ == "__main__":
+    layer = Linear(3, 54, bias = 1, data = 1.0)
+    x = Tensor([[3, 2, 1], [3, 2, 1]])
+    x = layer.forward(x)
+    print(x.shape)
--- maths.py
+++ maths.py
@@ -3,25 +3,3 @@ def softmax():
 
 def ReLU(a : list):
     return [max(0, i) for i in a]
-
-# Create a softmax function
-
-class Linear:
-    # modify it to include batches
-    def __init__(self, m : int, n : int, bias : bool = True, 
-                 a : float = 0):
-        self.m = m
-        self.n = n
-        self.w = [[a for i in range(n)] for j in range(m)]
-        self.b = [a for i in range(n)]
-        self.bias = bias
-    
-    def forward(self, x : list) -> list:
-        y = matmul(x, self.w)
-        return [y[0][i]+ self.bias * self.b[i] for i in range(self.n)]
-
-if __name__ == "__main__":
-    layer = Linear(3, 2, 1, 2)
-    x = [[3, 2, 1]]
-    print(layer.forward(x))
-    print(layer.w)
