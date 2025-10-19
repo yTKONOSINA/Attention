53054a1c8645f08a91dd08f3424a78176ace98a5
yTKONOSINA <tsutskov6@gmail.com>
2025-10-19 21:22:38 +0300

    made progress on tokenizer

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from transformers import BertTokenizer, AutoModel
+from transformers import AutoTokenizer
 from embedding import embed
 from attention import Attention
 
 
 sentences = [
-    "the man went to the [MASK] . he bought a gallon of milk.",
-    "i wrote this passage with a [MASK]"
+#    "the man went to the [MASK] . he bought a gallon of milk.",
+#    "i wrote this passage with a [MASK]",
+    "how are [MASK]?"
 ]
-# max token length is 512
-tokenizer = BertTokenizer.from_pretrained("prajjwal1/bert-tiny")
-for sentence in sentences:
-    tokens = tokenizer.tokenize(sentence,
-                                    add_special_tokens=True) # [CLS], [SEP]
-token_typs = [[0] * len(each sentence)]
 
+# Loading the model and the tokenizer
+model_name = "prajjwal1/bert-tiny"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+# max token length is 512
+for sentence in sentences:
+    # Tokenizer
+    # Return a batch (# sentences, # different lengths)
+    tokens = tokenizer(sentence, return_tensors = "pt")
+
+
+    print(tokens)
+    token_type_ids = [[0] * len(sentence)]
+
+# Get embeddings
+# 1. First, get the weights from the model (the only part where we have to 
+# use the tranformer library)
+# Get 
 model = AutoModel.from_pretrained("prajjwal1/bert-tiny")
 embedding_weights = model.embeddings.word_embeddings.weight.detach().cpu().numpy().tolist()  # shape (30522,128)
+
+# Get the results 
